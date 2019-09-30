d124247470699805dc811f25843fdc44eacd7ba3
Travis Tomka <underoathosx@gmail.com>
2019-09-30 09:01:41 -0600

    Initial commit.

---
+++ README.md
@@ -0,0 +1,11 @@
+## Project-X DDev
+
+Integrate the DDev environment into the project-x ecosystem. Which allows control of the environment state per project, these commands are consistent regardless of what environment type you've configured. This allows other plugins to interact with these commands, as they have a predictable API that can be extended.
+
+### Getting Started
+
+First, you'll need to download the project-x ddev library using composer:
+
+```bash
+composer require --dev pr0ject-x/px-ddev
+```
---
+++ src/ProjectX/Plugin/EnvironmentType/DDevEnvironmentType.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace Pr0jectX\PxDDev\ProjectX\Plugin\EnvironmentType;
+
+use Droath\ProjectX\ProjectX\Plugin\EnvironmentType\EnvironmentTypeBase;
+use Droath\RoboDDev\Task\loadTasks as ddevTasks;
+
+/**
+ * Define the DDev environment type.
+ */
+class DDevEnvironmentType extends EnvironmentTypeBase
+{
+    use ddevTasks;
+
+    /**
+     * {@inheritDoc}
+     */
+    public static function pluginId()
+    {
+        return 'ddev';
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public static function pluginLabel()
+    {
+        return 'DDev';
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function init()
+    {
+        $this->taskDDevConfig()->run();
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function start()
+    {
+        $this->taskDDevStart()->run();
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function stop()
+    {
+        $this->taskDDevStop()->run();
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function restart()
+    {
+        $this->taskDDevRestart()->run();
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function destroy()
+    {
+        $this->taskDDevDelete()->run();
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function info()
+    {
+        $this->taskDDevDescribe()->run();
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function ssh()
+    {
+        $this->taskDDevSsh()->run();
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function exec($cmd)
+    {
+        $this->taskDDevExec()->cmd($cmd)->run();
+    }
+}
